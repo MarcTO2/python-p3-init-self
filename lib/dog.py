22070dd9bb6428dc4b86f364ebd85a45f7a951f7
Absalom <mrclabsalom@gmail.com>
2023-08-29 20:09:53 +0300

    Complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed='mutt'):
+        self.name = name
+        self.breed = breed
+
+
+
+Fido = Dog('Fido', 'Spitz')
+Fido.name
+Fido.breed
+
+
+Snoopy = Dog('Snoopy')
